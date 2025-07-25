de13ea0253b47a0cafa5d4adfe1a986ceba1c662
HanSoJaa <harithmirza5@gmail.com>
2025-07-25 23:55:41 +0800

    chore: add debug logging for Cloudinary config

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -1,11 +1,19 @@
 import {v2 as cloudinary} from "cloudinary"
 
 const connectCloudinary = async () => {
+    console.log('Cloudinary config:', {
+      CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
+      CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
+      CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET
+    });
+
     cloudinary.config({
         cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
         api_key: process.env.CLOUDINARY_API_KEY,
         api_secret: process.env.CLOUDINARY_API_SECRET
     })
+
+    console.log("Cloudinary connected successfully");
 }
 
 export default connectCloudinary
